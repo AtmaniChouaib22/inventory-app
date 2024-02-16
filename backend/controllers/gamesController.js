bca1112c387f8d2ce6baabc7b6f109732e205351
Atmani Chouaib <atmanichouaib22@gmail.com>
2024-02-16 17:52:30 +0100

    added picture handling

--- backend/controllers/gamesController.js
+++ backend/controllers/gamesController.js
@@ -8,7 +8,7 @@ const games_list = asyncHandler(async (req, res, next) => {
   const allgames = await games
     .find({})
     .populate("genreName")
-    .populate("developper")
+    .populate("developer")
     .exec();
   res.status(200).json(allgames);
 });
@@ -20,24 +20,24 @@ const game_one = asyncHandler(async (req, res, next) => {
     return res.status(404).json({ err: "no game found" });
   const game = await games.findById(id);
   if (!game) return res.status(404).json({ err: "no game found" });
-  res.status(400).json(game);
+  res.status(200).json(game);
 });
 
 //game post
 const game_post = asyncHandler(async (req, res, next) => {
   console.log(req.file.filename);
   const picture = req.file.filename;
-  const { title, description, developper, genreName } = req.body;
+  const { title, description, developer, genreName } = req.body;
   const newGame = {
     title,
     description,
-    developper,
+    developer,
     genreName,
     picture,
   };
   if (!newGame) return res.status(404).json({ err: "no game found" });
   const game = await games.create(newGame);
-  res.status(400).json(game);
+  res.status(201).json(game);
 });
 
 //genre delete
@@ -47,7 +47,7 @@ const game_delete = asyncHandler(async (req, res, next) => {
     return res.status(404).json({ err: "no game found" });
   const game = await games.findByIdAndDelete(id);
   if (!game) return res.status(404).json({ err: "no game found" });
-  res.status(400).json(game);
+  res.status(200).json(game);
 });
 
 module.exports = { game_one, games_list, game_delete, game_post };
--- backend/models/gameSchema.js
+++ backend/models/gameSchema.js
@@ -2,17 +2,20 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-const gameSchema = new Schema({
-  title: { type: String, required: true, minLength: 3, maxLength: 25 },
-  genreName: { type: mongoose.Types.ObjectId, ref: "genres", required: true },
-  description: { type: String, required: true, maxLength: 100 },
-  developper: {
-    type: mongoose.Types.ObjectId,
-    ref: "developpers",
-    required: true,
+const gameSchema = new Schema(
+  {
+    title: { type: String, required: true, minLength: 3, maxLength: 25 },
+    genreName: { type: mongoose.Types.ObjectId, ref: "genres", required: true },
+    description: { type: String, required: true, maxLength: 100 },
+    developer: {
+      type: mongoose.Types.ObjectId,
+      ref: "developers",
+      required: true,
+    },
+    picture: { type: String },
   },
-  picture: { type: String },
-});
+  { timestamps: true }
+);
 
 gameSchema.virtual("url").get(function () {
   return `/api/games/${this._id}`;
