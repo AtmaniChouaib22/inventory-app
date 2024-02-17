c5a232bd7351f18f5bb01842291edd6b3e9a815e
Atmani Chouaib <atmanichouaib22@gmail.com>
2024-02-17 09:54:58 +0100

    dev/genres views

--- app/src/pages/Developers.jsx
+++ app/src/pages/Developers.jsx
@@ -1,5 +1,29 @@
+import { useState, useEffect } from "react";
 const Developers = () => {
-  return <div>developers page</div>;
+  const [devs, setDevs] = useState([]);
+  useEffect(() => {
+    const fetchDevs = async () => {
+      const response = await fetch("http://localhost:3000/api/developers");
+      if (response.ok) {
+        const json = await response.json();
+        setDevs(json);
+      }
+    };
+    fetchDevs();
+  }, []);
+  return (
+    <div className="flex flex-col items-center pt-10 gap-5">
+      <h1 className="text-3xl font-bold text-indigo-600">All Genres</h1>
+      <ul className="flex flex-col gap-3 items-start">
+        {devs.map((dev) => (
+          <li key={dev.developerId} className="font-semibold">
+            <span className="text-lg font-bold">{dev.developerName}</span>:{" "}
+            {dev.gameCount} Games
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Developers;
--- app/src/pages/Genres.jsx
+++ app/src/pages/Genres.jsx
@@ -1,5 +1,29 @@
+import { useState, useEffect } from "react";
 const Genres = () => {
-  return <div>all Genres</div>;
+  const [genres, setGenres] = useState([]);
+  useEffect(() => {
+    const fetchGenres = async () => {
+      const response = await fetch("http://localhost:3000/api/genres");
+      if (response.ok) {
+        const json = await response.json();
+        setGenres(json);
+      }
+    };
+    fetchGenres();
+  }, []);
+  return (
+    <div className="flex flex-col items-center pt-10 gap-5">
+      <h1 className="text-3xl font-bold text-indigo-600">All Genres List </h1>
+      <ul className="flex flex-col gap-3 items-start">
+        {genres.map((genre) => (
+          <li key={genre.genreId} className="font-semibold">
+            <span className="text-lg font-bold">{genre.genreName}</span>:{" "}
+            {genre.gameCount} Games
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Genres;
