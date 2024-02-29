450a96f72ccc283f635d097c3fda5f1eeb832f4b
Atmani Chouaib <atmanichouaib22@gmail.com>
2024-02-29 22:45:03 +0100

    edited home page ui

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,5 +1,16 @@
 const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <div className="flex justify-center items-center pt-20 flex-col gap-5">
+      <h1 className="text-4xl">welcome to your games inventory</h1>
+      <ul>
+        <li>
+          # you can add, view,delete, update(soon) game, genre , developer
+        </li>
+        <li># associate a game to genre and developer</li>
+        <li># view game count for every genre and developer</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
