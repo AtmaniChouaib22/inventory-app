6e5d37d0feaf10c29fbaef3dd5bd9b343ebcad2f
Atmani Chouaib <atmanichouaib22@gmail.com>
2024-03-01 23:40:24 +0100

    home page edited

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,13 +1,15 @@
 const Home = () => {
   return (
-    <div className="flex justify-center items-center pt-20 flex-col gap-5">
+    <div className="flex justify-center items-center pt-20 flex-col gap-5 pb-60">
       <h1 className="text-4xl">welcome to your games inventory</h1>
       <ul>
-        <li>
-          # you can add, view,delete, update(soon) game, genre , developer
-        </li>
+        <li># you can add, view,delete, update game, genre , developer</li>
         <li># associate a game to genre and developer</li>
         <li># view game count for every genre and developer</li>
+        <li># all game data is required </li>
+        <li>
+          # only genres and developers will show on the dev and genres pages
+        </li>
       </ul>
     </div>
   );
